0667fe7b77aa7a6387315f7ff9e15e8ed5d2da4e
Arch_axu <51283363+arch-yzk@users.noreply.github.com>
2019-12-16 10:59:33 +0900

    Add files via upload

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    //exp関数をRPN形式の文字列に変換する
+    let exp = "6.1 5.2 4.3 * + 3.4 2.5 / 1.6 * -";
+
+    //rpn関数で上記を計算する
+    let ans = rpn(exp);
+
+    //デバッグビルド時のみチェックする
+    //小数点以下4桁までを文字列に変換
+    debug_assert_eq!("26.2840", format!("{:.4}", ans)); // format!("", )が文字列変換？
+
+    println!("{} = {:.4}", exp, ans);
 }
+
+fn rpn(exp: &str) -> f64
+{
+    //Vecでスタック「stack」を定義
+    let mut stack = Vec::new();
+
+    //expをスペースで分割、tokenを順に束縛
+    for token in exp.split_whitespace()
+    {
+        //tokenがf64ならスタックに積む
+        if let Ok(num) = token.parse::<f64>()
+        {
+            stack.push(num);
+        }
+        else
+        {
+            //演算子かどうか
+            match token
+            {
+                //演算子ならapply2関数で計算
+                "+" => apply2(&mut stack, |x, y| x + y),
+                "-" => apply2(&mut stack, |x, y| x - y),
+                "*" => apply2(&mut stack, |x, y| x * y),
+                "/" => apply2(&mut stack, |x, y| x / y),
+
+                //tokenが非演算子ならエラーを起こして強制終了
+                _ => panic!("unknown operator: {}", token),
+            }
+        }
+    }
+    //スタックから1つ取り出す 失敗したらエラーを起こして終了する
+    stack.pop().expect("Stack underflow")
+}
+
